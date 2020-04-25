6cd264a663db49be16cf3c66052cdaa525576f1b
dmelen18 <davidmelendez321@yahoo.com>
2020-04-24 18:36:37 -0700

    Updated ViewMenuUserB.jsp

--- GoGoFood/src/Controller/MainHomePage.java
+++ GoGoFood/src/Controller/MainHomePage.java
@@ -34,7 +34,7 @@ public class MainHomePage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws 
 	ServletException, IOException 
 	{
-		 request.getRequestDispatcher( "/MainHomePage.jsp" )
+		 request.getRequestDispatcher( "MainHomePage.jsp" )
          .forward( request, response );
 	}
 
